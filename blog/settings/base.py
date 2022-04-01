3fbdf89bb48249652a72021352da092c6c75ea19
Ricardo <ricardo.csantos0877@gmail.com>
2022-04-01 15:58:06 -0300

    feat: auth configs

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'article',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = 'article/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
