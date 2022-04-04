908ceaff0367db772cfff4c2829983290dce39e3
Ricardo <ricardo.csantos0877@gmail.com>
2022-04-04 16:03:20 -0300

    remove: form_valid method

--- article/forms.py
+++ article/forms.py
@@ -6,7 +6,3 @@ class ArticleForm(ModelForm):
 	class Meta:
 		model = Article
 		fields = ['title', 'text']
-
-		def form_valid(self, form):
-			form.instance.author = self.request.user
-			return super().form_valid(form)
