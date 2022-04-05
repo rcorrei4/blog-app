6595d6bda69d7cc84fbe80f211800722688e566f
Ricardo <ricardo.csantos0877@gmail.com>
2022-04-05 20:19:35 -0300

    feat: new urls

--- article/urls.py
+++ article/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.ArticleList.as_view(), name='article'),
-    path('new/', views.ArticleCreate.as_view(), name='new_article'),
-    path('<slug:slug>/', views.ArticleDetail.as_view(), name='article'),
-    path('<slug:slug>/edit', views.ArticleUpdate.as_view(), name='edit_article'),
-    path('<slug:slug>/delete', views.ArticleDelete.as_view(), name='delete_article')
+    path('', views.ArticleList.as_view(), name='articles'),
+    path('article/new/', views.ArticleCreate.as_view(), name='new_article'),
+    path('article/<slug:slug>/', views.ArticleDetail.as_view(), name='article'),
+    path('article/<slug:slug>/edit', views.ArticleUpdate.as_view(), name='edit_article'),
+    path('article/<slug:slug>/delete', views.ArticleDelete.as_view(), name='delete_article')
 ]
