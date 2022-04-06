66d0154a239dd742162e87634f951e17ca967dd1
Ricardo <ricardo.csantos0877@gmail.com>
2022-04-06 15:40:34 -0300

    feat: like article view

--- article/urls.py
+++ article/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('article/new/', views.ArticleCreate.as_view(), name='new_article'),
     path('article/<slug:slug>/', views.ArticleDetail.as_view(), name='article'),
     path('article/<slug:slug>/edit', views.ArticleUpdate.as_view(), name='edit_article'),
-    path('article/<slug:slug>/delete', views.ArticleDelete.as_view(), name='delete_article')
+    path('article/<slug:slug>/delete', views.ArticleDelete.as_view(), name='delete_article'),
+    path('article/<slug:slug>/like', views.like_article, name='like_article')
 ]
