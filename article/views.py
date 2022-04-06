6d5488cafc7d1d25471c06a24a272f45d26cdce5
Ricardo <ricardo.csantos0877@gmail.com>
2022-04-06 15:40:13 -0300

    feat: like article view

--- article/views.py
+++ article/views.py
@@ -1,9 +1,12 @@
+from django.http import JsonResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 
+from django.contrib.auth.models import User
 from .models import Article
 from .forms import ArticleForm
 
@@ -40,3 +43,17 @@ class ArticleDelete(DeleteView):
 	def get_queryset(self):
 		user = self.request.user
 		return Article.objects.filter(author=user)
+
+@login_required
+def like_article(request, slug):
+	user = get_object_or_404(User, username=request.user.username)
+	article = get_object_or_404(Article, slug=slug)
+
+	if Article.objects.filter(slug=slug, likes__in=[user]).count() == 0:
+		article.likes.add(user)
+
+		return JsonResponse({'result': 'liked'})
+	else:
+		article.likes.remove(user)
+
+		return JsonResponse({'result': 'unliked'})
