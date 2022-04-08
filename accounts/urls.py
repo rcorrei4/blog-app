a9782a7578cd9c56cb8382bda5de6747f89f4bb4
Ricardo <ricardo.csantos0877@gmail.com>
2022-04-08 20:05:33 -0300

    feat: profile url

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
 	path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-	path('register/', views.register_view, name='register'),
+	path('register/', views.RegisterCreate.as_view(), name='register'),
+	path('profile/<int:pk>', views.ProfileDetail.as_view(), name='profile')
 ]
