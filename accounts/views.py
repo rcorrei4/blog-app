d3bbce5fb02ce3d579c3a2474d2379ca28b85695
Ricardo <ricardo.csantos0877@gmail.com>
2022-04-08 20:05:16 -0300

    feat: RegisterCreate and ProfileDetail

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,15 @@
+from django.views.generic.detail import DetailView
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-def register_view(request):
-	form = UserCreationForm(request.POST)
+from accounts.models import User
 
-	if form.is_valid():
-		form.save()
+class RegisterCreate(CreateView):
+	form_class = UserCreationForm
+	success_url = reverse_lazy('login')
+	template_name = 'auth/user_form.html'
 
-		username = form.cleaned_data.get('username')
-		password = form.cleaned_data.get('password1')
-
-		user = authenticate(username=username, password=password)
-		login(request, user)
-
-		return redirect('article')
-
-	else:
-		form = UserCreationForm()
-	return render(request, 'registration/register.html', {'form': form})
+class ProfileDetail(DetailView):
+	model = User
