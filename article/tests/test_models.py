ad7113d3ceac33c6194df9bbe9d79c483816646d
Ricardo <ricardo.csantos0877@gmail.com>
2022-04-23 20:48:13 -0300

    feat: fix missing author on article create + test tag model

--- article/tests/test_models.py
+++ article/tests/test_models.py
@@ -1,17 +1,35 @@
 from django.test import TestCase
-from article.models import Article
+from accounts.models import User
+from article.models import Article, Tag
 
 class ArticleTestCase(TestCase):
 
-    @classmethod
-    def setUpTestData(cls):
-        cls.article1 = Article.objects.create(title="Django Testing", body="how to write django tests")
-        cls.article2 = Article.objects.create(title="Class-based views", body="class-based views updated tutorial")
+	@classmethod
+	def setUpTestData(cls):
+		cls.user = User.objects.create_user(username='testuser', password='12345')
+		cls.article1 = Article.objects.create(
+			title="Django Testing", 
+			body="how to write django tests", 
+			author=cls.user)
 
-    def test_model_str(self):
-        self.assertEqual(str(self.article1), "Django Testing")
-        self.assertEqual(str(self.article2), "Class-based views")
+		cls.article2 = Article.objects.create(
+			title="Class-based views", 
+			body="class-based views updated tutorial", 
+			author=cls.user)
 
-    def test_get_absolute_url(self):
-        self.assertEqual("/article/django-testing/", self.article1.get_absolute_url())
-        self.assertEqual("/article/class-based-views/", self.article2.get_absolute_url())
+	def test_model_str(self):
+		self.assertEqual(str(self.article1), "Django Testing")
+		self.assertEqual(str(self.article2), "Class-based views")
+
+	def test_get_absolute_url(self):
+		self.assertEqual("/article/django-testing/", self.article1.get_absolute_url())
+		self.assertEqual("/article/class-based-views/", self.article2.get_absolute_url())
+
+class TagTestCase(TestCase):
+
+	@classmethod
+	def setUpTestData(cls):
+		cls.tag = Tag.objects.create(tag='tagname')
+
+	def test_model_str(self):
+		self.assertEqual(str(self.tag), "tagname")
