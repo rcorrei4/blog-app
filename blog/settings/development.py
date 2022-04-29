7913bf7cd2c54f884e98161bc9b01a8897f26e16
Ricardo <ricardo.csantos0877@gmail.com>
2022-04-28 21:52:18 -0300

    feat: environment variables

--- blog/settings/development.py
+++ blog/settings/development.py
@@ -1,14 +1,19 @@
 from .base import *
+import environ
+
+# Read env variables
+env = environ.Env()
+env.read_env(str(BASE_DIR / ".env"))
 
 DEBUG = True
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",
-        "PORT": 5432,
+        "NAME": env('DB_NAME'),
+        "USER": env('DB_USER'),
+        "PASSWORD": env('DB_PASSWORD'),
+        "HOST": env('DB_HOST'),
+        "PORT": env('DB_PORT'),
     }
 }
