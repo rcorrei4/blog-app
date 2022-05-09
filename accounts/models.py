2f614c96d8f701b5591852915f42b1e61d0c97e5
Ricardo <ricardo.csantos0877@gmail.com>
2022-05-09 15:39:44 -0300

    feat: saved_articles field

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class User(AbstractUser):
 	following = models.ManyToManyField('User', related_name='user_following')
 	articles = models.ManyToManyField('article.Article', related_name='user_articles')
+	saved_articles = models.ManyToManyField('article.Article', related_name='saved_articles')
