dc55aaa39b69229cc53daf52d74ee201aa15a6b3
Ricardo <ricardo.csantos0877@gmail.com>
2022-05-09 15:40:23 -0300

    feat: get article cover image

--- article/models.py
+++ article/models.py
@@ -2,10 +2,11 @@ from accounts.models import User
 from django.db import models
 from django.urls import reverse
 from autoslug import AutoSlugField
+import markdown as md
 
 class Article(models.Model):
 	title = models.CharField(max_length=128)
-	body = models.TextField()
+	body = models.TextField(blank=True, null=True)
 	date = models.DateField(auto_now=True)
 	slug = AutoSlugField(populate_from='title', unique_with='title', null=False)
 	created_at = models.DateTimeField(auto_now_add=True)
@@ -22,6 +23,12 @@ class Article(models.Model):
 
 	def get_absolute_url(self):
 		return reverse("article", kwargs={"slug": self.slug})
+
+	def get_article_img(self):
+		markdown_text = md.markdown(self.body, extensions=['markdown.extensions.fenced_code'])
+		markdown_cuted = markdown_text[markdown_text.index('<img'):]
+	
+		return markdown_cuted[:markdown_cuted.index('/>')+2]
 
 class ArticleComment(models.Model):
 	author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment_author')
