105fbe13ef67509a92a1d13df568d3f49cac52c8
Ricardo <ricardo.csantos0877@gmail.com>
2022-05-10 23:22:55 -0300

    feat: test get_article_img function

--- article/tests/test_models.py
+++ article/tests/test_models.py
@@ -8,22 +8,27 @@ class ArticleTestCase(TestCase):
 	def setUpTestData(cls):
 		cls.user = User.objects.create_user(username='testuser', password='12345')
 		cls.article1 = Article.objects.create(
-			title="Django Testing", 
-			body="how to write django tests", 
+			title='Django Testing', 
+			body='how to write django tests <img alt="Homepage LibriumSwap" src="https://raw.githubusercontent.com/LibriumSwap/LibriumSwap/main/presentation.jpg" />', 
 			author=cls.user)
 
 		cls.article2 = Article.objects.create(
-			title="Class-based views", 
-			body="class-based views updated tutorial", 
+			title='Class-based views', 
+			body='class-based views updated tutorial', 
 			author=cls.user)
 
 	def test_model_str(self):
-		self.assertEqual(str(self.article1), "Django Testing")
-		self.assertEqual(str(self.article2), "Class-based views")
+		self.assertEqual(str(self.article1), 'Django Testing')
+		self.assertEqual(str(self.article2), 'Class-based views')
 
 	def test_get_absolute_url(self):
-		self.assertEqual("/article/django-testing/", self.article1.get_absolute_url())
-		self.assertEqual("/article/class-based-views/", self.article2.get_absolute_url())
+		self.assertEqual('/article/django-testing/', self.article1.get_absolute_url())
+		self.assertEqual('/article/class-based-views/', self.article2.get_absolute_url())
+
+	def test_get_article_img(self):
+		self.assertEqual('<img alt="Homepage LibriumSwap" src="https://raw.githubusercontent.com/LibriumSwap/LibriumSwap/main/presentation.jpg" />',
+						  self.article1.get_article_img())
+		self.assertEqual(False ,self.article2.get_article_img())
 
 class TagTestCase(TestCase):
 
@@ -32,4 +37,4 @@ class TagTestCase(TestCase):
 		cls.tag = Tag.objects.create(tag='tagname')
 
 	def test_model_str(self):
-		self.assertEqual(str(self.tag), "tagname")
+		self.assertEqual(str(self.tag), 'tagname')
