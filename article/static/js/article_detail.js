ba1358546b7e0229b7ee28c1a56f8c25110e3b12
Ricardo <ricardo.csantos0877@gmail.com>
2022-06-28 15:39:28 -0300

    feat: follow user on article detail page

--- article/static/js/article_detail.js
+++ article/static/js/article_detail.js
@@ -21,3 +21,17 @@ saveBtn.onclick = function () {
 		saveBtn.innerText = result.result
 	})
 }
+
+followBtn = document.querySelector('#follow-btn')
+
+if(followBtn != null) {
+	followBtn.onclick = function () {
+	fetch(`/accounts/follow/${followBtn.dataset.id}`, {
+		method: 'GET',
+	})
+	.then(response => response.json())
+	.then(result => {
+		followBtn.innerText = result.result
+	})
+}
+}
