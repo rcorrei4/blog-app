034bc86c27340db143f73779bba04d75fd14c934
rcorrei4 <ricardo.csantos0877@gmail.com>
2022-09-13 10:46:36 -0300

    feat: aullauth for OAuth2 authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-	path('register/', views.RegisterCreate.as_view(), name='register'),
 	path('follow/<int:pk>/', views.follow_user, name='follow_user'),
 ]
